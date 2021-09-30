83394364ff752a3fce36476b8c8e0aa66ad2d5ee
Crowdbotics <team@crowdbotics.com>
2021-09-30 14:09:15 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "faspascampus_30980",
+  url: "https://faspascampus_30980.botics.co",
+  api: "https://faspascampus_30980.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
